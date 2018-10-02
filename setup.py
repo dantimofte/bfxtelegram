e2b5850132fd8ad0b25716f0be1592bb5b8bb84b
Dan Timofte <timoftedan@gmail.com>
2018-10-02 15:02:46 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+
+# Runtime dependencies. See requirements.txt for development dependencies.
+DEPENDENCIES = [
+    "pandas",
+    "python-telegram-bot",
+    "wheel",
+    "pillow",
+    "selenium",
+    "bokeh",
+    "python-telegram-bot",
+    "bitfinex-v2"
+]
+
+setup(
+    name='bfxtelegram',
+    version=VERSION,
+    description='Control Bitfinex account with a Telegram bot',
+    author='Dan Timofte',
+    author_email='dantimofte@pm.me',
+    url='https://github.com/dantimofte/bfxtelegram',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=DEPENDENCIES,
+    keywords=['bitcoin', 'btc', 'iota', 'telegram', 'bitfinex'],
+    classifiers=[],
+    zip_safe=True,
+    entry_points={
+        "console_scripts": [
+            "bfxtelegram = bfxtelegram.__main__:main",
+        ],
+    },
+
+)
